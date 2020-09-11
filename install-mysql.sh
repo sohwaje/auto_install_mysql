357aff494aa64d714540b173ff229ea17635574a
sigongweb <kisssulran@i-screammedia.com>
2020-09-11 17:11:03 +0900

    updaTE

--- install-mysql.sh
+++ install-mysql.sh
@@ -222,30 +222,56 @@ temp_password() {
   echo "temporary password is : $password"
 }
 
+copy_start_script(){
+  sudo cp -ar $BASEDIR/support-files/mysql.server /etc/rc.d/init.d/mysql.server
+  sudo sed -i 's,^basedir=$,'"basedir=$BASEDIR"','  /etc/rc.d/init.d/mysql.server
+  sudo sed -i 's,^datadir=$,'"basedir=$MYSQL_DATA"',' /etc/rc.d/init.d/mysql.server
+  sudo bash -c "cat << EOF > /etc/systemd/system/mysql.service
+[Unit]
+Description=MySQL Server
+After=network.target
+
+[Service]
+Type= forking
+ExecStart = /etc/rc.d/init.d/mysql.server start
+ExecStop = /etc/rc.d/init.d/mysql.server stop
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+sudo systemctl daemon-reload && sudo systemctl start mysql.server && sudo systemctl enable mysql.server
+}
+
 ################################### start mysql ################################
+# start_mysql() {
+#   echo -e "\e[1;32;40m[9] Starting MySQL \e[0m"
+#   cd $BASEDIR
+#   sudo ./bin/mysqld_safe --defaults-file=/etc/my.cnf --user=mysql >/dev/null &
+#   sleep 100
+#   # sudo kill -9 $!
+#   COUNT=100
+#   while [[ $COUNT -gt 0 ]]
+#   do
+#     echo > /dev/tcp/127.0.0.1/3306 >/dev/null
+#     x=$?  # echo의 실행 값을 x에 대입. x의 값은 0 또는 1
+#     let COUNT=COUNT-1
+#   done
+# # if status value is $x 0 OK, else Failed
+#   if [[ $x -eq 0 ]];then
+#     echo -e "\e[1;33;40m [OK] \e[0m"
+#   else
+#     echo -e "\e[1;31;40m [Failed] \e[0m"
+#     exit 9
+#   fi
+#   sudo kill -TERM $! 1>/dev/null 2>&1  # 가장 최근 백그라운에서 실행된 프로세스를 종료한다. 표준 출력, 표준 에러 모두 보여주지 않는다.
+# }
 start_mysql() {
-  echo -e "\e[1;32;40m[9] Starting MySQL \e[0m"
-  cd $BASEDIR
-  sudo ./bin/mysqld_safe --defaults-file=/etc/my.cnf --user=mysql >/dev/null &
-  sleep 100
-  # sudo kill -9 $!
-  COUNT=100
-  while [[ $COUNT -gt 0 ]]
-  do
-    echo > /dev/tcp/127.0.0.1/3306 >/dev/null
-    x=$?  # echo의 실행 값을 x에 대입. x의 값은 0 또는 1
-    let COUNT=COUNT-1
-  done
-# if status value is $x 0 OK, else Failed
-  if [[ $x -eq 0 ]];then
-    echo -e "\e[1;33;40m [OK] \e[0m"
-  else
-    echo -e "\e[1;31;40m [Failed] \e[0m"
-    exit 9
-  fi
-  sudo kill -TERM $! 1>/dev/null 2>&1  # 가장 최근 백그라운에서 실행된 프로세스를 종료한다. 표준 출력, 표준 에러 모두 보여주지 않는다.
+  sudo systemctl start mysql.server
 }
 
 initialize_mysql
+copy_start_script
 temp_password
 start_mysql
+
+# ref: https://qastack.kr/ubuntu/76808/how-do-i-use-variables-in-a-sed-command
