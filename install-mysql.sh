af092c9dfd44b3fe8a3ed80853a558a68f946be1
sigongweb <kisssulran@i-screammedia.com>
2020-09-14 15:38:45 +0900

    update

--- install-mysql.sh
+++ install-mysql.sh
@@ -213,15 +213,15 @@ done
 ############################# download MySQL 5.7 ###############################
 echo -e "\e[1;32;40m[6] download MySQL5.7 \e[0m"
 sudo wget -P \
-  /tmp/ https://github.com/sohwaje/bbs/raw/master/mysql-5.7.31-linux-glibc2.12-x86_64.tar.gz &
+  /tmp/ https://github.com/sohwaje/bbs/raw/master/${INSTALLFILE}.tar.gz >& /dev/null
 
 
 ################## extract mysql-5.7.31-linux-glibc2.12-x86_64 #################
 echo -e "\e[1;32;40m[7] extracting \e[0m"
 sleep 1
 cd /tmp/
-sudo tar xvfz $INSTALLFILE.tar.gz && sudo mv $INSTALLFILE /usr/local/mysql && sudo rm -f $INSTALLFILE.tar.gz &
-echo
+sudo tar xvfz $INSTALLFILE.tar.gz && sudo mv $INSTALLFILE /usr/local/mysql && sudo rm -f $INSTALLFILE.tar.gz
+
 
 ################################# set permission ###############################
 sudo chown -R mysql.mysql $BASEDIR && sudo chown -R mysql.mysql $DATADIR
